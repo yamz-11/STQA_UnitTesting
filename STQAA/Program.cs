19540e31d8d9019de8ec76e3410154d2dadf1f91
yamz-11 <wiratamajrz@gmail.com>
2023-11-16 13:59:10 +0700

    first commit

---
+++ STQAA/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace UAS_PAW1
+{
+    public class BangunDatar
+    {
+
+        public double PersegiPanjang(double p, double l)
+        {
+            return p * l;
+        }
+        public double persegi(double p)
+        {
+            return p * p * p * p;
+        }
+        public double lingkaran(double l)
+        {
+            return 3.14 * (l * l);
+        }
+        public double segitiga(double p, double l)
+        {
+            return 0.5 * (p * l);
+        }
+        public double jajargenjang(double p, double z)
+        {
+            return 2 * (p + z);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double p = Double.Parse(args[0]);
+            double l = Double.Parse(args[1]);
+            double z = Double.Parse(args[2]);
+
+            BangunDatar bd = new BangunDatar();
+
+            double PP = bd.PersegiPanjang(p, l);
+            double PG = bd.persegi(p);
+            double LK = bd.lingkaran(l);
+            double ST = bd.segitiga(p, l);
+            double JG = bd.jajargenjang(p, z);
+
+            Console.WriteLine("Persegi Panjang = {0}", PP);
+            Console.WriteLine("Persegi = {0}", PG);
+            Console.WriteLine("Lingkaran = {0}", LK);
+            Console.WriteLine("Segitiga = {0}", ST);
+            Console.WriteLine("Jajar Genjang = {0}", JG);
+
+        }
+    }
+}
---
+++ STQAATests/BangunDatarTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UAS_PAW1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace UAS_PAW1.Tests
+{
+    [TestFixture()]
+    public class BangunDatarTests
+    {
+        BangunDatar bd = new BangunDatar();
+
+        [SetUp]
+        public void init()
+        {
+            bd = new BangunDatar();
+        }
+        [Test()]
+        public void PersegiPanjangTest()
+        {
+            Double HasilPP = bd.PersegiPanjang(4, 5);
+            Assert.AreEqual(20, HasilPP);
+        }
+
+        [Test()]
+        public void persegiTest()
+        {
+            Double HasilPG = bd.persegi(4);
+            Assert.AreEqual(256, HasilPG);
+        }
+
+        [Test()]
+        public void lingkaranTest()
+        {
+            Double HasilLK = bd.lingkaran(5);
+            Assert.AreEqual(78, 5, HasilLK);
+        }
+
+        [Test()]
+        public void segitigaTest()
+        {
+            Double HasilST = bd.segitiga(4, 5);
+            Assert.AreEqual(10, HasilST);
+        }
+
+        [Test()]
+        public void jajargenjangTest()
+        {
+            Double HasilJG = bd.jajargenjang(4, 6);
+            Assert.AreEqual(20, HasilJG);
+        }
+    }
+}
